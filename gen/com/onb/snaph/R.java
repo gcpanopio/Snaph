8f7b74132e8e635087856687e255200ca7929b90
Marlou Ramas <marlou.ramas@orangeandbronze.com>
2012-05-15 10:49:39 +0800

    commited again

--- gen/com/onb/snaph/R.java
+++ gen/com/onb/snaph/R.java
@@ -15,11 +15,9 @@ public final class R {
         public static final int white=0x7f050001;
     }
     public static final class drawable {
-        public static final int close=0x7f020000;
-        public static final int default_image=0x7f020001;
-        public static final int facebook_icon=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int snaph_launcher=0x7f020004;
+        public static final int default_image=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int snaph_launcher=0x7f020002;
     }
     public static final class id {
         public static final int cancel_upload=0x7f07000a;
