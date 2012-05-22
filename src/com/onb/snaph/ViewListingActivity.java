ed1335cc866599c57a6e03ce6e05b99cf265346d
Marlou Ramas <marlou.ramas@orangeandbronze.com>
2012-05-22 20:17:04 +0800

    no progress at all

--- src/com/onb/snaph/ViewListingActivity.java
+++ src/com/onb/snaph/ViewListingActivity.java
@@ -1,5 +1,15 @@
 package com.onb.snaph;
 
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.http.AccessToken;
+
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook.DialogListener;
 import com.facebook.android.FacebookError;
@@ -27,7 +37,7 @@ import android.widget.Toast;
  */
 public class ViewListingActivity extends Activity{
 
-	protected static final String TAG = ViewListingActivity.class.getSimpleName();
+	protected static final String TAG = ViewListingActivity.class.getSimpleName().toString();
 	
 	private ImageView image;
 	private TextView title;
@@ -39,11 +49,14 @@ public class ViewListingActivity extends Activity{
 	private SharedPreferences prefs;
 	private String listingLink;
 	private String imageLink;
+	private OAuthConsumer consumer; 
+	private OAuthProvider provider;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
     	
         super.onCreate(savedInstanceState);
+        this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
         setContentView(R.layout.view_listing);
         
         Button edit= (Button) findViewById(R.id.edit_button);
@@ -146,32 +159,32 @@ public class ViewListingActivity extends Activity{
 	 * @param view
 	 */
 	public void onFacebookPost(View view){
-			Bundle params = new Bundle();
-			params.putString("link", listingLink);
-			params.putString("picture", imageLink);
-			params.putString("name", title.getText().toString());
-			params.putString("description", description.getText().toString());
-			params.putString("caption", price.getText().toString());
-		 	snaph.getFacebook().dialog(this, "feed", params, new DialogListener(){
+		Bundle params = new Bundle();
+		params.putString("link", listingLink);
+		params.putString("picture", imageLink);
+		params.putString("name", title.getText().toString());
+		params.putString("description", description.getText().toString());
+		params.putString("caption", price.getText().toString());
+	 	snaph.getFacebook().dialog(this, "feed", params, new DialogListener(){
 
 			public void onComplete(Bundle values) {
 				final String postId = values.getString("post_id");
-
+	
 		        if (postId != null) {
 		        	Log.d(TAG,"Wall Post Success");
 		        } else {
 		        	Log.d(TAG,"Wall Post Failed");
 		        }
 			}
-
+	
 			public void onFacebookError(FacebookError e) {
 				Log.d(TAG,e.getMessage());
 			}
-
+	
 			public void onError(DialogError e) {
 				Log.d(TAG,e.getMessage());
 			}
-
+	
 			public void onCancel() {}
 		 });
 	}
@@ -181,43 +194,69 @@ public class ViewListingActivity extends Activity{
 	 * @param view
 	 */
 	public void onTwitterPost(View view){
-		this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (TwitterUtils.isAuthenticated(prefs)) {
-			sendTweet();
-		} 
-		else {
-			Intent i = new Intent(getApplicationContext(), PrepareRequestTokenActivity.class);
-			i.putExtra("tweet_msg",getTweetMsg());
-			startActivity(i);
-		}
-	}
-	
-	private String getTweetMsg() {
-		return "Selling " + title.getText() + " for " + price.getText();
-	}	
-	
-	/**
-	 * 
-	 */
-	public void sendTweet() {
-		Thread t = new Thread() {
-	        public void run() {
-	        	
-	        	try {
-	        		TwitterUtils.sendTweet(prefs,getTweetMsg());
-	        		twitterHandler.post(mUpdateTwitterNotification);
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-	        }
+		Log.d(TAG,"Tweeting");
+		String token = prefs.getString(OAuth.OAUTH_TOKEN, "");
+		String secret = prefs.getString(OAuth.OAUTH_TOKEN_SECRET, "");
+		Log.d(TAG,"AccessToken");
+		AccessToken a = new AccessToken(token,secret);
+		Log.d(TAG,"TweeterFactory");
+		Twitter twitter = new TwitterFactory().getInstance();
+		Log.d(TAG,"SettingUp");
+		twitter.setOAuthConsumer(SnaphApplication.CONSUMER_KEY, SnaphApplication.CONSUMER_SECRET);
+		twitter.setOAuthAccessToken(a);
+		Log.d(TAG,"Authenticated");
+		boolean authenticated = false;
+		/*
+		try {
+			twitter.getAccountSettings();
+			authenticated = true;
+		} catch (TwitterException e) {}
+		Log.d(TAG,authenticated+"");
+		*/
+		if (authenticated) {
+			Thread t = new Thread() {
+		        public void run() {
+		        	try {
+		        		String token = prefs.getString(OAuth.OAUTH_TOKEN, "");
+		        		String secret = prefs.getString(OAuth.OAUTH_TOKEN_SECRET, "");
+		        		
+		        		AccessToken a = new AccessToken(token,secret);
+		        		Twitter twitter = new TwitterFactory().getInstance();
+		        		twitter.setOAuthConsumer(SnaphApplication.CONSUMER_KEY, 
+		        				SnaphApplication.CONSUMER_SECRET);
+		        		twitter.setOAuthAccessToken(a);
+		        		String msg = "Check out this link to see the item I'm selling \n" + listingLink;
+		                twitter.updateStatus(msg);
+		        		twitterHandler.post(mUpdateTwitterNotification);
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+		        }
 
-	    };
-	    t.start();
+		    };
+		    t.start();
+		    try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	} 
+		else {
+			try {
+	    		consumer = new CommonsHttpOAuthConsumer(SnaphApplication.CONSUMER_KEY, SnaphApplication.CONSUMER_SECRET);
+	    	    provider = new CommonsHttpOAuthProvider(SnaphApplication.REQUEST_URL,SnaphApplication.ACCESS_URL,SnaphApplication.AUTHORIZE_URL);
+	    	} catch (Exception e) {
+	    		Log.e(TAG, "Error creating consumer / provider",e);
+			}
+	        Log.i(TAG, "Starting task to retrieve request token.");
+	        new OAuthRequestTokenTask(this,consumer,provider).execute();
+    	}
 	}
 	
 	final Runnable mUpdateTwitterNotification = new Runnable() {
-		public void run() {
-        	Toast.makeText(getBaseContext(), "Tweet sent !", Toast.LENGTH_LONG).show();
+        public void run() {
+        	Toast.makeText(getBaseContext(), "Tweet sent!", Toast.LENGTH_LONG).show();
         }
     };
+
 }
